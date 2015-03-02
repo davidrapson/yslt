052548bddc46bf5a0225aae03ea393f7eff78d37
David Rapson <david@davidrapson.co.uk>
2015-03-02 21:32:36 +0000

    Don't grab data as part of build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-wget "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20data.html.cssselect%20WHERE%20url%3D%27http%3A%2F%2Fwww.metacritic.com%2Fbrowse%2Falbums%2Fscore%2Fmetascore%2Fyear%3Fyear_selected%3D2015%27%20AND%20css%3D%27.product_row%27%20limit%2050&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=" -O data/albums.json
 jspm bundle-sfx --minify lib/main.jsx! build/app.js
 ./node_modules/csso/bin/csso -i css/style.css -o css/style.min.css
